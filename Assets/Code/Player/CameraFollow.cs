ee5051c3054973d1a77e9345aa3ed47adb835d00
artem4567815 <99493172+artem4567815@users.noreply.github.com>
2024-05-10 00:19:29 +0300

    Наложил тестовые спрайты, немного поменял управление камерой, сделал поворот на 360 градусов

--- Assets/Code/Player/CameraFollow.cs
+++ Assets/Code/Player/CameraFollow.cs
@@ -8,28 +8,22 @@ public class CameraFollow : MonoBehaviour
     public Transform followTransform;
     public BoxCollider2D mapBounds;
 
-    private float xMin, xMax, yMin, yMax;
-    private float camY,camX;
+    private float yMin, yMax;
+    private float camY;
     private float camOrthsize;
-    private float cameraRatio;
     private Camera mainCam;
 
     private void Start()
     {
-        xMin = mapBounds.bounds.min.x;
-        xMax = mapBounds.bounds.max.x;
         yMin = mapBounds.bounds.min.y;
         yMax = mapBounds.bounds.max.y;
         mainCam = GetComponent<Camera>();
-        camOrthsize = mainCam.orthographicSize;
-        cameraRatio = (xMax + camOrthsize) / 2.0f;
     }
     
     void FixedUpdate()
     {
-        camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
-        camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
-        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
+        camY = Mathf.Clamp(followTransform.position.y, yMin - camOrthsize, yMax + camOrthsize);
+        this.transform.position = new Vector3(0f, camY, this.transform.position.z);
         
         
     }
--- Assets/Code/Player/PlayerScript.cs
+++ Assets/Code/Player/PlayerScript.cs
@@ -33,7 +33,10 @@ namespace Assets.Code.Player
         private void Update()
         {
             // Запрос на реализацию поворота
-            _rotateScript.OnRotate(Input.mousePosition.x);
+            //_rotateScript.OnRotate(Input.mousePosition.x);
+            Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotateZ = Mathf.Atan2(diference.y, diference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotateZ - 90);
         }
         private void FixedUpdate() {
             _move.OnWalk();
