f1fe59a07c679ec94bd6c74a1844eeac34fb76c1
artem4567815 <99493172+artem4567815@users.noreply.github.com>
2024-05-11 18:17:22 +0300

    Удалил папку с NavMesh

---
+++ Assets/Code/Other/AudioController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource musicController;
+    public AudioClip music;
+    void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        musicController.PlayOneShot(music);
+    }
+}
